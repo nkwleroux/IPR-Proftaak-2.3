aac1e7284154c2c3adef52d1ae605ceab5471590
Nicholas <nkw.leroux@student.avans.nl>
2021-04-08 15:32:42 +0200

    Fixed CMakeLists and includes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # IPR-Proftaak-2.3
+
+Dit project is gemaakt voor de IPR van 2.3 voor het vak IMC Proftaak.
--- main/main.c
+++ main/main.c
@@ -6,30 +6,24 @@
 #include "driver/i2c.h"
 #include "esp_log.h"
 #include "sdkconfig.h"
-#include "esp32/rom/uart.h"
 
-//LCD
-#include "smbus.h"
-#include "i2c-lcd1602.h"
-
-//Button 2
+//Touch buttons
 #include "esp_peripherals.h"
 #include "input_key_service.h"
-#include "periph_adc_button.h"
 
 #undef USE_STDIN
 
-#define I2C_MASTER_NUM           I2C_NUM_0
-#define I2C_MASTER_TX_BUF_LEN    0                     // disabled
-#define I2C_MASTER_RX_BUF_LEN    0                     // disabled
-#define I2C_MASTER_FREQ_HZ       100000
-#define I2C_MASTER_SDA_IO        CONFIG_I2C_MASTER_SDA
-#define I2C_MASTER_SCL_IO        CONFIG_I2C_MASTER_SCL
-#define LCD_NUM_ROWS			 4
-#define LCD_NUM_COLUMNS			 40
-#define LCD_NUM_VIS_COLUMNS		 20
+// #define I2C_MASTER_NUM           I2C_NUM_0
+// #define I2C_MASTER_TX_BUF_LEN    0                     // disabled
+// #define I2C_MASTER_RX_BUF_LEN    0                     // disabled
+// #define I2C_MASTER_FREQ_HZ       100000
+// #define I2C_MASTER_SDA_IO        CONFIG_I2C_MASTER_SDA
+// #define I2C_MASTER_SCL_IO        CONFIG_I2C_MASTER_SCL
+// #define LCD_NUM_ROWS			 4
+// #define LCD_NUM_COLUMNS			 40
+// #define LCD_NUM_VIS_COLUMNS		 20
 
-#define MAINTAG "menu"
+#define MAINTAG "TouchButton"
 #define BUTTONTAG "touchpad button"
 
 char *btn_states[] = {  "INPUT_KEY_SERVICE_ACTION_UNKNOWN",        /*!< unknown action id */
@@ -56,7 +50,7 @@ void app_main(void){
     key_serv_info.handle = set;
     periph_service_handle_t input_key_handle = input_key_service_create(&key_serv_info);
     AUDIO_NULL_CHECK(BUTTONTAG, input_key_handle, return);
-    input_key_service_add_key(input_key_handle, input_info, INPUT_KEY_NUM); //INPUT_KEY_NUM = 4 (the touch pads on ESP32-LyraT board)
+    input_key_service_add_key(input_key_handle, input_info, INPUT_KEY_NUM); //INPUT_KEY_NUM = 6 (the touch pads on ESP32-LyraT board)
     periph_service_set_callback(input_key_handle, input_key_service_cb, NULL);
 }
 
@@ -64,30 +58,33 @@ void app_main(void){
 static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_service_event_t *evt, void *ctx)
 {
     ESP_LOGD(BUTTONTAG, "type=%d, source=%d, data=%d, len=%d", evt->type, (int)evt->source, (int)evt->data, evt->len);
-    switch ((int)evt->data) {
+    switch ((int)evt->data) {   
+        case INPUT_KEY_USER_ID_REC:
+
+            ESP_LOGI(BUTTONTAG, "[ * ] [rec] %s",btn_states[evt->type]);
+            break;
+        case  INPUT_KEY_USER_ID_MODE:
+ 
+            ESP_LOGI(BUTTONTAG, "[ * ] [mode] %s",btn_states[evt->type]);
+            break;
         case INPUT_KEY_USER_ID_PLAY:
-            //if (evt->type == INPUT_KEY_SERVICE_ACTION_PRESS) {
-            //ESP_LOGI(BUTTONTAG, "[ * ] [play] %s",btn_states[evt->type]);
-            //}
-            ESP_LOGI(BUTTONTAG, "[ * ] [set] %s",btn_states[evt->type]);
+  
+            ESP_LOGI(BUTTONTAG, "[ * ] [play] %s",btn_states[evt->type]);
             break;
         case INPUT_KEY_USER_ID_SET:
-            // if (evt->type == INPUT_KEY_SERVICE_ACTION_PRESS) {
-            //     ESP_LOGI(BUTTONTAG, "[ * ] [ID SET] %d",evt->type);
-            // }
+ 
             ESP_LOGI(BUTTONTAG, "[ * ] [set] %s",btn_states[evt->type]);
             break;
         case INPUT_KEY_USER_ID_VOLDOWN:
-            if (evt->type == INPUT_KEY_SERVICE_ACTION_CLICK) {
-                
-            }
+
             ESP_LOGI(BUTTONTAG, "[ * ] [volume down] %s",btn_states[evt->type]);
             break;
         case INPUT_KEY_USER_ID_VOLUP:
-            if (evt->type == INPUT_KEY_SERVICE_ACTION_CLICK) {
+            // if (evt->type == INPUT_KEY_SERVICE_ACTION_CLICK) {
 
-            }
+            // }
             ESP_LOGI(BUTTONTAG, "[ * ] [volume up] %s",btn_states[evt->type]);
+            break;
         default:
             break;
     }
